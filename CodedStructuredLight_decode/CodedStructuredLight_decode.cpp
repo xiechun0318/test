a2cfe618e3fbc531328fea1070383a9db55a0cc3
xiech_000 <xiech_000@SASAI_ITS>
2017-04-27 19:24:34 +0900

    プロジェクト ファイルを追加します。

---
+++ CodedStructuredLight_decode/CodedStructuredLight_decode.cpp
@@ -0,0 +1,46 @@
+// CodedStructuredLight_decode.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+using namespace cv;
+using namespace std;
+
+Mat1b computeShadowMask(const Mat1b& blackImage, const Mat1b& whiteImage, size_t threshold)
+{
+	Mat1b shadowMask = Mat1b::zeros(blackImage.size());
+	for (int j = 0; j < shadowMask.rows; ++j) {
+		for (int i = 0; i < shadowMask.cols; ++i) {
+			if (whiteImage(j, i) > blackImage(j, i) + threshold) {
+				shadowMask(j, i) = 255;
+			}
+		}
+	}
+	return shadowMask;
+}
+
+Mat2f computeDecodeImage(Ptr<structured_light::GrayCodePattern>& graycode, const vector<Mat1b>& captured_pattern, const Mat1b& mask)
+{
+	Mat2f decodedImage = Mat2f::zeros(mask.size());
+	for (int j = 0; j < decodedImage.rows; ++j) {
+		for (int i = 0; i < decodedImage.cols; ++i) {
+			if (mask(j, i) == 0) {
+				continue;
+			}
+			Point projPixel;
+			bool error = graycode->getProjPixel(captured_pattern, i, j, projPixel);
+			if (error) {
+				continue;
+			}
+			decodedImage(j, i)[0] = (float)projPixel.x;
+			decodedImage(j, i)[1] = (float)projPixel.y;
+		}
+	}
+	return decodedImage;
+}
+
+int main()
+{
+    return 0;
+}
+
---
+++ CodedStructuredLight_decode/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+#include "opencv2/opencv.hpp"
+#include "opencv2\world.hpp"
+#include <fstream>
+
+
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください
